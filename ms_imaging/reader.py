c5adce565fe036846e95ba2b0eb14f5a034c7f17
neilswainston <neil.swainston@manchester.ac.uk>
2018-07-18 11:03:21 +0100

    Added clustering.

--- ms_imaging/reader.py
+++ ms_imaging/reader.py
@@ -9,6 +9,10 @@ To view a copy of this license, visit <http://opensource.org/licenses/MIT/>.
 '''
 # pylint: disable=invalid-name
 import sys
+
+from sklearn.cluster import MiniBatchKMeans
+
+import numpy as np
 import pandas as pd
 
 
@@ -33,9 +37,29 @@ def read(filename):
     return pd.DataFrame(data, columns=['x', 'y', 'mz', 'i'])
 
 
+def cluster(df, n_clusters=384, verbose=0):
+    '''Cluster.'''
+    kmeans = MiniBatchKMeans(n_clusters=n_clusters,
+                             init_size=n_clusters + 1,
+                             verbose=verbose).fit(df[['x', 'y']])
+
+    df['cluster'] = kmeans.labels_
+
+    centres = zip(*kmeans.cluster_centers_)
+    df['cluster_centre_x'] = df['cluster'].apply(lambda x: centres[0][x])
+    df['cluster_centre_y'] = df['cluster'].apply(lambda y: centres[1][y])
+    df['distance_from_centre'] = \
+        np.linalg.norm(df[['x', 'y']].values -
+                       df[['cluster_centre_x', 'cluster_centre_y']].values,
+                       axis=1)
+
+    return df
+
+
 def main(args):
     '''main method.'''
     df = read(args[0])
+    df = cluster(df, verbose=1)
     df.to_csv(args[1], index=False)
 
 
